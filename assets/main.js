1381f78a70e73aad0d9f424849d052b8ed09f207
antaGB <antapermana1156@gmail.com>
2023-07-23 20:08:25 +0800

    redesign

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,28 @@
 $(window).ready(function () {
-  $(".loader").addClass("active");
+  // loader
+  $(".loader")
+    .delay(1000)
+    .queue(function (next) {
+      $(".loader").addClass("remove");
+
+      next();
+    });
+
+  // toggle navbar
+  $("#nav-btn").click(function () {
+    // open and close mobile navbar
+    $("#nav-links").toggleClass("active");
+
+    // if the navbar is open, body overflow is hidden
+    if ($("#nav-links").hasClass("active")) {
+      $("body").css("overflow", "hidden");
+    } else {
+      $("body").css("overflow", "auto");
+    }
+  });
+
+  $(".nav-link").click(function () {
+    $("#nav-links").removeClass("active");
+    $("body").css("overflow", "auto");
+  });
 });
